1c4cf4a8a4312df48061f6c94a777a4b6a4d2067
Milena Figueredo <milena.silva@zup.com.br>
2019-07-05 18:13:18 -0300

    Adicionar modelo de onibus

---
+++ sistemaCompras/src/main/java/br/com/passagens/models/OnibusModel.java
@@ -0,0 +1,89 @@
+package br.com.passagens.models;
+
+public class OnibusModel {
+
+	private String empresa;
+	private String destino;
+	private String tempoDeViagem;
+	private int lugares;
+	private boolean lugaresDisponiveis;
+//	private List<PassagensModel> cliente;
+
+	public OnibusModel() {
+	}
+
+	public OnibusModel(String empresa, String destino, String tempoDeViagem, int lugares, boolean lugaresDisponiveis,
+			List<PassagensModel> cliente) {
+		super();
+		this.empresa = empresa;
+		this.destino = destino;
+		this.tempoDeViagem = tempoDeViagem;
+		this.lugares = lugares;
+		this.lugaresDisponiveis = lugaresDisponiveis;
+//		this.cliente = cliente;
+	}
+
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+
+	public String getDestino() {
+		return destino;
+	}
+
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+
+	public String getTempoDeViagem() {
+		return tempoDeViagem;
+	}
+
+	public void setTempoDeViagem(String tempoDeViagem) {
+		this.tempoDeViagem = tempoDeViagem;
+	}
+
+	public int getLugares() {
+		return lugares;
+	}
+
+	public void setLugares(int lugares) {
+		this.lugares = lugares;
+	}
+
+	public boolean isLugaresDisponiveis() {
+		return lugaresDisponiveis;
+	}
+
+	public void setLugaresDisponiveis(boolean lugaresDisponiveis) {
+		this.lugaresDisponiveis = lugaresDisponiveis;
+	}
+
+//	public List<PassagensModel> getCliente() {
+//		return cliente;
+//	}
+//
+//	public void setCliente(List<PassagensModel> cliente) {
+//		this.cliente = cliente;
+//	}
+
+	@Override
+	public String toString() {
+
+		StringBuilder modelo = new StringBuilder();
+
+		modelo.append("Empresa" + this.empresa);
+		modelo.append("Destino" + this.destino);
+		modelo.append("TempoDeViagem" + this.tempoDeViagem);
+		modelo.append("Lugares" + this.lugares);
+		modelo.append("Quantidade de lugares" + this.lugares);
+		modelo.append("Vazio" + this.isLugaresDisponiveis());
+
+		return modelo.toString();
+	}
+
+}
