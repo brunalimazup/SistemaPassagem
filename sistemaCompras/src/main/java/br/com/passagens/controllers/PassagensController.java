94a382742f58617f77b923d0849195707d0e9e2a
Milena Figueredo <milena.silva@zup.com.br>
2019-07-08 11:39:22 -0300

    Adicionar correção na logica de compra

--- sistemaCompras/src/main/java/br/com/passagens/controllers/PassagensController.java
+++ sistemaCompras/src/main/java/br/com/passagens/controllers/PassagensController.java
@@ -1,8 +1,37 @@
 package br.com.passagens.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.passagens.models.PassagensModel;
+import br.com.passagens.services.PassagensService;
 
 @Controller
 public class PassagensController {
 
+	@Autowired
+	PassagensService passagensService;
+
+	@GetMapping
+	public ModelAndView mostarEmpresas() {
+		ModelAndView modelAndView = new ModelAndView("Onibus.html");
+		modelAndView.addObject("", "");
+		return modelAndView;
+	}
+
+	@PostMapping("/compra")
+	public String SalvarCadastro(PassagensModel passagensModel) {
+		passagensService.salvarCompra(passagensModel);
+		return "redirect:/";
+	}
+
+	@GetMapping("/compra")
+	public ModelAndView mostrarCadastro(PassagensModel passagensModel) {
+		ModelAndView modelAndView = new ModelAndView("cadastro.html");
+		return modelAndView;
+
+}
 }
--- sistemaCompras/src/main/java/br/com/passagens/services/PassagensService.java
+++ sistemaCompras/src/main/java/br/com/passagens/services/PassagensService.java
@@ -1,14 +1,43 @@
 package br.com.passagens.services;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import org.springframework.stereotype.Service;
+
+import br.com.passagens.models.OnibusModel;
 import br.com.passagens.models.PassagensModel;
 
 @Service
 public class PassagensService {
-	
-	private List<PassagensModel> clientes = new ArrayList<PassagensModel>(); {{
-		clientes.add(new PassagensModel("Milena", "email", 20, 2));
-	}};
+
+	private List<PassagensModel> clientes = new ArrayList<PassagensModel>();
+	{
+		{
+			clientes.add(new PassagensModel("Milena", "email", 20, 2));
+		}
+	};
+
+	private List<OnibusModel> onibus = new ArrayList<OnibusModel>();
+	{
+		{
+			onibus.add(new OnibusModel("Cometa", "Rio", "2 horas", 20));
+			onibus.add(new OnibusModel("Estrela", "Santos", "1 hora e 30 minutos", 20));
+		}
+	}
+
+	private Collection<OnibusModel> mostrarEmpresas() {
+		return this.onibus;
+	}
+
+	public String salvarCompra(PassagensModel passagensModel) {
+		String resposta = "Compra realizada.";
+		if (passagensModel.getIdade() >= 18) {
+			clientes.add(passagensModel);
+		} else {
+			resposta = "Você não possue idade para realizar essa compra.";
+		}
+		return resposta;
+	}
 
 }
