2f658de92df867440bd3563c4a7872e2e494f042
Milena Figueredo <milena.silva@zup.com.br>
2019-07-08 16:35:23 -0300

    Adicionar parte da logica

--- sistemaCompras/src/main/java/br/com/passagens/controllers/PassagensController.java
+++ sistemaCompras/src/main/java/br/com/passagens/controllers/PassagensController.java
@@ -1,14 +1,12 @@
 package br.com.passagens.controllers;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.com.passagens.models.OnibusModel;
 import br.com.passagens.models.PassagensModel;
 import br.com.passagens.services.PassagensService;
 
@@ -18,7 +16,7 @@ public class PassagensController {
 	@Autowired
 	PassagensService passagensService;
 
-	@GetMapping ("/")
+	@GetMapping("/")
 	public ModelAndView mostarEmpresas() {
 		ModelAndView modelAndView = new ModelAndView("Onibus.html");
 		modelAndView.addObject("onibus", passagensService.mostrarEmpresas());
@@ -26,9 +24,8 @@ public class PassagensController {
 	}
 
 	@PostMapping("/compra")
-	public String SalvarCadastro(PassagensModel passagensModel) {
+	public void SalvarCadastro(PassagensModel passagensModel) {
 		passagensService.salvarCompra(passagensModel);
-		return "redirect:/";
 	}
 
 	@GetMapping("/compra")
@@ -36,5 +33,6 @@ public class PassagensController {
 		ModelAndView modelAndView = new ModelAndView("cadastro.html");
 		return modelAndView;
 
-	}	
+	}
+
 }
